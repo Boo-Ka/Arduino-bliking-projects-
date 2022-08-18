c855b299b03f05bbe3f9a67da0566c466c506c72
Boopathi G <111436524+Boo-Ka@users.noreply.github.com>
2022-08-18 22:45:15 +0530

    Update Blinking.ino

--- Blinking.ino
+++ Blinking.ino
@@ -4,5 +4,13 @@ Name: Boopathi */
 
 void setup()
 { 
-p
+pinMode(5, OUTPUT); //led interfacing with arduino pin
+}
 
+void loop()
+  {
+  digtalWrite(5,HIGH);  //Output for 5th pin for on condition
+  delay(1000); // time delay (1000=1sec)
+  digtalWrite(5,LOW); //output for 5th pin for off condition
+  delay(1000);
+  }
